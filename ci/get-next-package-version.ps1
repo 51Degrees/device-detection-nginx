601e059bd52eec86d1317c12bc4319045403ecdc
Ben51Degrees <ben@51degrees.com>
2023-04-27 11:58:31 +0100

    FEAT: Added support for NGINX 1.23.2. This means a change to how cookie values are handled.

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -1,10 +1,12 @@
 
 param (
     [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
     [string]$VariableName
 )
 
-./steps/get-next-package-version.ps1 -RepoName "device-detection-nginx-test" -VariableName "GitVersion"
+./steps/get-next-package-version.ps1 -RepoName $RepoName -VariableName "GitVersion"
 
 Write-Output "Setting '$VariableName' to '$($GitVersion.SemVer)'"
 Set-Variable -Name $VariableName -Value $GitVersion.SemVer -Scope Global
