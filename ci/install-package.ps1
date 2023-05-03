8b2eb2d59d9045ebfb357e11ca259048b4cfd81d
ben51degrees <ben@51degrees.com>
2023-05-03 16:31:03 +0100

    Update install-package.ps1

--- ci/install-package.ps1
+++ ci/install-package.ps1
@@ -9,11 +9,13 @@ param(
 # Combine the current working directory with the repository name
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
+$ModuleName = "ngx_http_51D_module.so"
+
 # Define the path for downloaded artifacts
-$PackagePath = [IO.Path]::Combine($RepoPath, "package", "package_$Name")
+$PackagePath = [IO.Path]::Combine($RepoPath, "package", "package_$Name", $ModuleName)
 
 # Get the local install path
-$InstallPath = [IO.Path]::Combine($RepoPath, "build", "modules")
+$InstallPath = [IO.Path]::Combine($RepoPath, "build", "modules", $ModuleName)
 
 # Display the repository path and enter it
 Write-Output "Entering '$RepoPath'"
@@ -24,7 +26,7 @@ try {
     Write-Output "Install NGINX '$NginxVersion' without 51Degrees module"
     make install-no-module FIFTYONEDEGREES_NGINX_VERSION=$NginxVersion
 
-    Write-Output "Copying the 51Degrees module"
+    Write-Output "Copying the 51Degrees module from '$PackagePath' to '$InstallPath'"
     Copy-Item -Path $PackagePath -Destination $InstallPath
 
 }
