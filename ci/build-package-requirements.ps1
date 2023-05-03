6aca1cc7a7137ba1c58185df39a54d246203eb8e
ben51degrees <ben@51degrees.com>
2023-05-03 16:18:25 +0100

    Update build-package-requirements.ps1

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -1,8 +1,8 @@
 param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
-    [string]$Name = "1.21.3_dynamic",
-    [string]$NginxVersion = "1.21.3"
+    [string]$Name,
+    [string]$NginxVersion
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
@@ -16,11 +16,11 @@ try {
         
     # Create a directory for binary files from which they will be uploaded
     # as artifacts.
-    New-Item -path "$RepoPath/package-files/$NginxVersion" -ItemType Directory -Force 
+    New-Item -path "$RepoPath/package-files" -ItemType Directory -Force 
     
     $ModuleName = "ngx_http_51D_module.so"
     $InputFile = [IO.Path]::Combine($RepoPath, "build", "modules", $ModuleName)
-    $OutputFile = [IO.Path]::Combine($RepoPath, "package-files", $NginxVersion, $ModuleName)
+    $OutputFile = [IO.Path]::Combine($RepoPath, "package-files", $ModuleName)
 
     # Copy module
     Copy-Item -Path $InputFile -Destination $OutputFile
