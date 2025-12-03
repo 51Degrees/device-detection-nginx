2bfa781ff871919de0ffb59eac0676d621d4efe3
Alexander Popel <alexander.popel@postindustria.com>
2025-12-03 11:12:51 +0200

    ci: use stable relative path in fetch-assets

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,5 +1,4 @@
 param (
-    [Parameter(Mandatory)][string]$RepoName,
     [string]$DeviceDetection,
     [string]$DeviceDetectionUrl,
     [Parameter(Mandatory)][string]$NginxKey,
@@ -8,19 +7,16 @@ param (
 )
 $ErrorActionPreference = "Stop"
 
-$deviceDetectionData = "$RepoName/device-detection-cxx/device-detection-data"
-
-./steps/fetch-assets.ps1 -DeviceDetection:$DeviceDetection -DeviceDetectionUrl:$DeviceDetectionUrl `
-    -Assets "TAC-HashV41.hash", "20000 Evidence Records.yml", "20000 User Agents.csv"
-
-New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/TAC-HashV41.hash" -Path "$deviceDetectionData/TAC-HashV41.hash"
-New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/20000 Evidence Records.yml" -Path "$deviceDetectionData/20000 Evidence Records.yml"
-New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/20000 User Agents.csv" -Path "$deviceDetectionData/20000 User Agents.csv"
+$assets = "TAC-HashV41.hash", "20000 Evidence Records.yml", "20000 User Agents.csv"
+./steps/fetch-assets.ps1 -DeviceDetection:$DeviceDetection -DeviceDetectionUrl:$DeviceDetectionUrl -Assets $assets
+foreach ($asset in $assets) {
+    New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/$asset" -Path "$PSScriptRoot/../device-detection-cxx/device-detection-data/$asset"
+}
 
 # Write the key and certificate files for NGINX Plus so it can be installed.
 Write-Host "Writing NGINX Plus repo key"
-Write-Output $NginxKey > "$RepoName/nginx-repo.key"
+$NginxKey > "$PSScriptRoot/../nginx-repo.key"
 Write-Host "Writing NGINX Plus repo certificate"
-Write-Output $NginxCert > "$RepoName/nginx-repo.crt"
+$NginxCert > "$PSScriptRoot/../nginx-repo.crt"
 Write-Host "Writing NGINX Plus Jwt token"
-Write-Output $NginxJwtToken > "$RepoName/license.jwt"
+$NginxJwtToken > "$PSScriptRoot/../license.jwt"
