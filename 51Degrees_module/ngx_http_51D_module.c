9750fee97c720b1b9fdc800b95938bd00ce5814f
Automation51D <60396166+Automation51D@users.noreply.github.com>
2026-01-07 04:07:40 +0100

    Merged Pull Request '#343 update-copyright->main: Update copyright'
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- 51Degrees_module/ngx_http_51D_module.c
+++ 51Degrees_module/ngx_http_51D_module.c
@@ -1,6 +1,6 @@
 /* *********************************************************************
  * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
- * Copyright 2025 51 Degrees Mobile Experts Limited, Davidson House,
+ * Copyright 2026 51 Degrees Mobile Experts Limited, Davidson House,
  * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
  *
  * This Original Work is licensed under the European Union Public Licence
