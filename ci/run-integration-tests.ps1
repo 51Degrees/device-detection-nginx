f5c82f200dc4b11e8107aeb007c353dc37f51676
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2026-03-17 16:51:09 +0200

    Merged Pull Request '#356 better-test-error-handling->main: ci: improve test error handling'

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -1,31 +1,20 @@
-
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
     [string]$Name = "1.21.3_dynamic",
     [string]$NginxVersion = "1.21.3",
     [string]$BuildMethod = "dynamic"
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
-
-Write-Output "Entering '$RepoPath'"
-Push-Location $RepoPath
-
+Push-Location $PSScriptRoot/..
 try {
+    # Create the output directory
+    $results = New-Item -ItemType directory -Force -Path test-results/integration
 
-    # Create the output directories if they don't already exist.
-    if ($(Test-Path -Path "test-results") -eq  $False) {
-        mkdir test-results
-    }
-    if ($(Test-Path -Path "test-results/integration") -eq  $False) {
-        mkdir test-results/integration
-    }
-    
     # Disable leak checks here, as they are for the unit tests.
     $env:ASAN_OPTIONS="detect_odr_violation=0:detect_leaks=0"
 
-    Write-Output "Setting up the CDN example"
+    Write-Host "Setting up the CDN example"
     # Copy the example config
     Copy-Item examples/hash/jsExample/javascript.conf build/nginx.conf
     # Set the data file
@@ -36,38 +25,29 @@ try {
     ./nginx
 
     # Run the selenium tests
-    $JsExamplePath = [IO.Path]::Combine($RepoPath, "tests", "examples", "jsExample")
-    Write-Output "Entering '$JsExamplePath'"
-    Push-Location $JsExamplePath
-
+    Push-Location "$PSScriptRoot/../tests/examples/jsExample"
     try {
-        Write-Output "Running CDN tests"
+        Write-Host "Running CDN tests"
         $env:PATH="$($env:PATH):$pwd/driver"
         npm install
         npx browserslist@latest --update-db
-        npm test
+        & {
+            $PSNativeCommandUseErrorActionPreference = $false
+            npm test
+        }
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host '::warning::CDN tests exited with 1, ignoring due to frequent false negatives'
+        }
     }
     finally {
-        
-        Write-Output "Leaving '$JsExamplePath'"
         Pop-Location
-        Write-Output "Stopping NGINX"
+        Write-Host "Stopping NGINX"
         ./nginx -s quit
-        if ($LASTEXITCODE -ne 0 ) {
-            Write-Error "Failed to exit Nginx"
-            exit 1
-        }
-
     }
 
     # Test the examples
-    Write-Output "Testing examples"
-    make test-examples FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$RepoPath/test-results/integration/$($Name)_Examples.xml
-
-}
-finally {
-
-    Write-Output "Leaving '$RepoPath'"
+    Write-Host "Testing examples"
+    make test-examples FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$results/${Name}_Examples.xml
+} finally {
     Pop-Location
-
 }
--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -1,74 +1,50 @@
-
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
     [string]$Name = "1.21.3_dynamic",
     [string]$NginxVersion = "1.21.3",
     [string]$BuildMethod = "dynamic",
     [bool]$FullTests = $False,
     [bool]$MemCheck = $False
 )
+$ErrorActionPreference = "Stop"
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+$failed = $false
 
-Write-Output "Entering '$RepoPath'"
-Push-Location $RepoPath
-
+$repo = (Get-Item $PSScriptRoot/..).FullName
+Push-Location $repo
 try {
+    # Create the output directory
+    $results = New-Item -ItemType directory -Force -Path test-results/unit
 
-    # Create the output directories if they don't exist already.
-    if ($(Test-Path -Path "test-results") -eq  $False) {
-        mkdir test-results
-    }
-    if ($(Test-Path -Path "test-results/unit") -eq  $False) {
-        mkdir test-results/unit
-    }
-
-    Write-Output "Running 51Degrees unit tests"
-    make test FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$RepoPath/test-results/unit/$Name.xml
-    # Keep track of failed tests
-    $Passed = $($LASTEXITCODE -eq 0)
+    Write-Host "Running 51Degrees unit tests"
+    make test FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$results/$Name.xml || $($failed = $true)
     # Output the full results file, as some bits are not reported in GitHub
-    cat $RepoPath/test-results/unit/$Name.xml
+    Get-Content -Raw $results/$Name.xml
 
     if ($FullTests -eq $True) {
         $env:ALLOW_PASSING_TODOS = 'yep' # don't consider Nginx's todo tests as failures
 
         # If full tests are requested, then run the full test suite (NGINX core tests, with the module enabled)
-        Write-Output "Running full NGINX unit tests"
-        make test-full FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$RepoPath/test-results/unit/$($Name)_Full.xml
-        $Passed = $($Passed -and $LASTEXITCODE -eq 0)
+        Write-Host "Running full NGINX unit tests"
+        make test-full FIFTYONEDEGREES_DATAFILE=TAC-HashV41.hash FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$results/${Name}_Full.xml || $($failed = $true)
 
         # Now do the same with the NGINX Plus executable
-        Write-Output "Running full NGINX unit tests against NGINX Plus"
+        Write-Host "Running full NGINX unit tests against NGINX Plus"
 
         # For mgmt module we need to set permissions to nginx-mgmt-state
         sudo chmod -R a+xrw /var/lib
 
         # As we're not using the makefile, we need to set the environment variables ourselves, and add common path to license_token
         $env:TEST_NGINX_BINARY="/usr/sbin/nginx"
-        $env:TEST_NGINX_GLOBALS="mgmt {license_token /etc/nginx/license.jwt;} load_module $RepoPath/build/modules/ngx_http_51D_module.so;"
-        $env:TEST_NGINX_GLOBALS_HTTP="51D_file_path $RepoPath/device-detection-cxx/device-detection-data/TAC-HashV41.hash;"
-        prove --formatter TAP::Formatter::JUnit -v tests/51degrees.t tests/nginx-tests :: TAC-HashV41.hash > $RepoPath/test-results/unit/$($Name)_Full_Plus.xml
-        $Passed = $($Passed -and $LASTEXITCODE -eq 0)
+        $env:TEST_NGINX_GLOBALS="mgmt {license_token /etc/nginx/license.jwt;} load_module $repo/build/modules/ngx_http_51D_module.so;"
+        $env:TEST_NGINX_GLOBALS_HTTP="51D_file_path $repo/device-detection-cxx/device-detection-data/TAC-HashV41.hash;"
+        prove --formatter TAP::Formatter::JUnit -v tests/51degrees.t tests/nginx-tests :: TAC-HashV41.hash > $results/${Name}_Full_Plus.xml || $($failed = $true)
         # Unset the environment variables for future tests.
         $env:TEST_NGINX_BINARY=""
         $env:TEST_NGINX_GLOBALS=""
         $env:TEST_NGINX_GLOBALS_HTTP=""
-
     }
-
-}
-finally {
-
-    Write-Output "Leaving '$RepoPath'"
+} finally {
     Pop-Location
-
 }
 
-if ($Passed) {
-    exit 0
-}
-else {
-    exit 1
-}
+exit $failed ? 1 : 0
