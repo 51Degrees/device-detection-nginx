e8390ae8bc12404254d5a1840496349585bdc1bc
Maksym Kucherov <maximus.the0@gmail.com>
2026-09-04 15:11:47 +0200

    CI: Migrate scripts in `ci/` from `FIFTYONEDEGREES_DATAFILE` to `51DEGREES_DD_PATH`. pt.4

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -56,9 +56,12 @@ try {
     $staticBuild = $BuildMethod -eq 'static' ? 'STATIC_BUILD=1' : $null
     Write-Host "Looking for HASH file(s)..."
     Get-ChildItem -Recurse -Filter 'TAC-HashV41.hash'
-    Write-Host "Testing examples"
 
-    $HashFilePath = (Get-ChildItem "$PSScriptRoot/../assets/TAC-HashV41.hash" | Select-Object -ExpandProperty FullName)
+    $HashFilePath = (Get-ChildItem "$PSScriptRoot/../../assets/TAC-HashV41.hash" | Select-Object -ExpandProperty FullName)
+    Write-Host "Checking ($HashFilePath)..."
+    Get-ChildItem "$HashFilePath"
+
+    Write-Host "Testing examples"
     make test-examples $staticBuild DONT_CLEAN_TESTS=1 51DEGREES_DD_PATH="$HashFilePath" FIFTYONEDEGREES_FORMATTER='--formatter TAP::Formatter::JUnit' FIFTYONEDEGREES_TEST_OUTPUT=$results/${Name}_Examples.xml
 } finally {
     Pop-Location
