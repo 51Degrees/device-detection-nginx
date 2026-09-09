63937b0d5d4ad1a5ba0207bb60e8da72c08e7d2b
Maksym Kucherov <maximus.the0@gmail.com>
2026-09-09 12:28:26 +0200

    CI: Download Asn file from Azure.

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -9,3 +9,20 @@ $assets = "TAC-HashV41.hash", "20000 Evidence Records.yml", "20000 User Agents.c
 foreach ($asset in $assets) {
     New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/$asset" -Path "$PSScriptRoot/../device-detection-cxx/device-detection-data/$asset"
 }
+
+Push-Location "$PSScriptRoot/../ip-intelligence-cxx/ip-intelligence-data"
+try {
+    Write-Host "Entering $PWD"
+    # Remove old Asn file (if exists)
+    $AsnFilePath = "51Degrees-IPIV4AsnIpiV41.ipi"
+    if (Test-Path -Type Leaf -Path $AsnFilePath) {
+        Remove-Item -Path $AsnFilePath
+        Write-Host "Deleted $AsnFilePath"
+    }
+    
+    Write-Host "Loading free IPI data files..."
+    & ./get-lite-file-from-azure.ps1
+} finally {
+    Write-Host "Leaving $PWD"
+    Pop-Location
+}
